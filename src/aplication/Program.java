dff34cb2ae25eae6bd66e7c286248b0229a0a660
aline <aline.soares.2@hotmail.com>
2020-08-11 19:42:39 -0300

    Criando a classe Program

---
+++ src/aplication/Program.java
@@ -0,0 +1,22 @@
+package aplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import entities.TaxaImposto;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<TaxaImposto> list = new ArrayList<>();
+		
+		sc.close();
+
+	}
+
+}
