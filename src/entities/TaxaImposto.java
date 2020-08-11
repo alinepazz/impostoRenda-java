dd9ebfaae1a50c459eed4604f5de8251701f83c4
aline <aline.soares.2@hotmail.com>
2020-08-11 18:37:15 -0300

    Criando a classe TaxaImposto

---
+++ src/entities/TaxaImposto.java
@@ -0,0 +1,31 @@
+package entities;
+
+public abstract class TaxaImposto {
+
+	private String nome;
+	private Double rendaAnual;
+	
+	public TaxaImposto(String nome, Double rendaAnual) {
+		this.nome = nome;
+		this.rendaAnual = rendaAnual;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getRendaAnual() {
+		return rendaAnual;
+	}
+
+	public void setRendaAnual(Double rendaAnual) {
+		this.rendaAnual = rendaAnual;
+	}
+	
+	
+	
+}
